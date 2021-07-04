0119433251bebc119fd2ace366943825818ec668
nasernikzad <n.nikzad@pomtech.com>
2021-07-04 08:36:23 +0430

    changes

--- server.js
+++ server.js
@@ -12,21 +12,20 @@ var bodyParser = require("body-parser");
 
 const Schema = mongoose.Schema;
 
-const userSchema = Schema({
-  _id: { type: Schema.Types.ObjectId},
+const userSchema = new Schema({
+  _id: Schema.Types.ObjectId,
   username: String,
-  exercises: [{ type: Schema.Types.ObjectId, ref: 'Exercise' }]
+  reviews: [{ type: Schema.Types.ObjectId, ref: 'Exercise' }],
 });
 
-const exerciseSchema = Schema({
-  author: { type: Schema.Types.ObjectId, ref: 'User' },
+const exerciseSchema = new Schema({
   description: String,
   duration: Number,
   date: Date,
 });
 
-const Exercise = mongoose.model('Exercise', exerciseSchema);
-const User = mongoose.model('User', userSchema);
+const ExerciseModel = mongoose.model('Exercise', exerciseSchema);
+const UserModel  = mongoose.model('User', userSchema);
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -41,7 +40,10 @@ app.post('/api/users', function(req, res) {
   // console.log('given user name', username);
 
   // Store user in mongo db.
-  var userRecord = new User({name: username});
+  var userRecord = new UserModel({
+    username: username, 
+    _id: new mongoose.Types.ObjectId()
+  });
 
   userRecord.save(function(err, data) {
     if (err) return console.error(err);
@@ -54,7 +56,7 @@ app.post('/api/users', function(req, res) {
 // You can make a GET request to /api/users to get an array of all users. Each element in the array is an object containing a user's username and _id.
 app.get('/api/users', function(req, res) {
 
-  User.find(function(err, data) {
+  UserModel.find(function(err, data) {
     if (err) return console.error(err);
     // The returned response will be an object with username and _id properties.
     res.json(data);
@@ -81,6 +83,8 @@ app.post('/api/users/:_id/exercises', function(req, res) {
   console.log('given user duration', duration);
   console.log('given user date', date);
 
+  
+
   // Store user in mongo db.
   var exerciseRecord = new Exercise({
     fk_user: user_id,
@@ -105,23 +109,25 @@ app.get('/api/users/:_id/logs', function(req, res) {
 
   console.log('your input ', req.params._id);
 
-  User.findById(req.params._id).populate('exercises').exec(function(err, user) {
-    console.log('2')
-    return res.json(user);
+  User.findOne({ _id: req.params._id })
+  .populate('reviews')
+  .then((result) => {
+    res.json(result);
+  })
+  .catch((error) => {
+    res.status(500).json({ error });
   });
-
   console.log('3')
-
 });
 
 const listener = app.listen(process.env.PORT, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 
-  User.findById('60e03f86b341974b3086dd28').populate('exercises').exec(function(err, user) {
-    console.log('4')
-    console.log(user)
-    // return res.json(user);
-  });
+  // UserModel.findById('60e03f86b341974b3086dd28').populate('exercises').exec(function(err, user) {
+  //   console.log('4')
+  //   console.log(user)
+  //   // return res.json(user);
+  // });
 
   // Exercise.findOne({ title: Nintendo }).populate('_creator')
 
